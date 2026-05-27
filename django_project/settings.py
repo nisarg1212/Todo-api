2d061e4c79d811e7f44632044fc2ddbd1307ddc4
Nisarg Bhatt <nisargbhatt48@gmail.com>
2026-05-28 03:49:42 +0530

    adding files

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,13 +38,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd Part
+    'rest_framework',
+    'corsheaders',
+
     # Local
     'todos.apps.TodosConfig',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -52,7 +63,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
 ROOT_URLCONF = 'django_project.urls'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 TEMPLATES = [
     {
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Todo
+
+from .serializers import TodoSerializer
+
+from rest_framework import generics
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
